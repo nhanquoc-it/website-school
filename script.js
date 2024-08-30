5dccc5b93e4810519d72231f1c6c33061b985113
nhanquoc-it <caphuunhanquoc2510@gmail.com>
2024-08-30 15:56:08 +0700

    01-Xây dựng phần header

---
+++ script.js
@@ -0,0 +1,11 @@
+// Toggle Open & Close Menu Mobile
+var openMenuBar = document.querySelector(".navbar-toggle-mobile");
+var closeMenuBar = document.querySelector(".navbar-close-mobile");
+openMenuBar.addEventListener("click", function () {
+  document.querySelector(".navbar").style.transform = "translateX(0%)";
+  document.querySelector(".navbar").style.opacity = "1";
+});
+closeMenuBar.addEventListener("click", function () {
+  document.querySelector(".navbar").style.transform = "translateX(100%)";
+  document.querySelector(".navbar").style.opacity = "0";
+});
